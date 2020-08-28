a94e47a73a30b59bc3e915435fa1ace92cff23e9
Tine <csteinmeier@fh-bielefeld.de>
2020-08-28 11:50:54 +0200

    Create sketch_2sliders.ino
    added folder and sketch

---
+++ arduino/sketch_2sliders.ino
@@ -0,0 +1,17 @@
+
+int sensorValueA = 0;        // Potentiometer A value
+int sensorValueB = 0;        // Potentiometer B value
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValueA = analogRead(7);
+  sensorValueB = analogRead(6);
+  Serial.print("a:");
+  Serial.print(sensorValueA);
+  Serial.print("b:");
+  Serial.print(sensorValueB);
+  delay(1000);                // waits for a second
+}
